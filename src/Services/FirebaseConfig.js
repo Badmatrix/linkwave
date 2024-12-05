2d3431b2080779baa57647509942c2fefd2c5964
Badmatrix <matrix.og11@gmail.com>
2024-12-05 23:51:25 +0100

    FIREBASE_API_KEY configuration

--- src/Services/FirebaseConfig.js
+++ src/Services/FirebaseConfig.js
@@ -1,17 +1,16 @@
-/* eslint-disable no-undef */
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "linkwave-1ffda.firebaseapp.com",
-  projectId: "linkwave-1ffda",
-  storageBucket: "linkwave-1ffda.appspot.com",
-  messagingSenderId: "109499012411",
-  appId: "1:109499012411:web:dd3ac677fe19b0537bd31e",
-  measurementId: "G-663W42P5VW",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAG_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MESUREMENT_ID,
 };
 
 // Initialize Firebase
