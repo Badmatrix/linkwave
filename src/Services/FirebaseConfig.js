d8ac2a629d6f6e13d03e8c9b939cd0f4acd7c1eb
Badmatrix <matrix.og11@gmail.com>
2024-12-06 08:41:06 +0100

    API KEY

--- src/Services/FirebaseConfig.js
+++ src/Services/FirebaseConfig.js
@@ -3,24 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAG_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-// measurementId: import.meta.env.VITE_FIREBASE_MESUREMENT_ID,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCrNy00TJdybfS0jr4iH7pw3SJ29edgKdw",
-  authDomain: "linkwave-1ffda.firebaseapp.com",
-  projectId: "linkwave-1ffda",
-  storageBucket: "linkwave-1ffda.firebasestorage.app",
-  messagingSenderId: "109499012411",
-  appId: "1:109499012411:web:dd3ac677fe19b0537bd31e",
-  measurementId: "G-663W42P5VW",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAG_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+measurementId: import.meta.env.VITE_FIREBASE_MESUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
